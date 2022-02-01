6f046443743c6a36fd5f72dc976343f29d3613ab
wangqinghuaa <451416191@qq.com>
2022-02-01 23:40:30 +0800

    web_test

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ import pytest
 # pytest.main(["-s","-v","-m","smoke","--html=Outputs/report.html","--reruns","2","--reruns-delay","5"])
 
 # 使用allure报告生成
-pytest.main(["-s","-v","--html=Outputs/reports/pytest.html",
-                  "--alluredir=Outputs/allure"])
+# pytest.main(["-s","-v","--html=Outputs/reports/pytest.html",
+#                   "--alluredir=Outputs/allure"])
+
+pytest.main(["-s","-v","--alluredir=Outputs/allure"])
 
 
