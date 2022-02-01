04c456ed6c9d04302d40ba60e04a1c01a88e5d04
wangqinghuaa <451416191@qq.com>
2022-02-01 23:57:07 +0800

    web_test

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import pytest
 
 # pytest.main(["-s","-v","--alluredir=Outputs/allure"])
 
-pytest.main(["--reruns", "2", "--reruns-delay", "5", "-s","v", "--junitxml=Outputs/reports/report.xml",
+pytest.main(["--reruns", "2", "--reruns-delay", "5", "-s","-v", "--junitxml=Outputs/reports/report.xml",
               "--html=Outputs/reports/html_report.html", "--alluredir=Outputs/allure_reports"])
 
 
